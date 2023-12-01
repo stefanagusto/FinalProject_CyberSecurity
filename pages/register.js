fe1ae516bad1308043a6b14ac18caa1e741cdc28
Stefan Agusto Hutapea <stefanhutapea@gmail.com>
2023-12-01 21:48:23 +0700

    front end - fix login page - add register page

--- pages/register.js
+++ pages/register.js
@@ -1,16 +1,31 @@
 // pages/register.js
-import React from 'react';
-import 'tailwindcss/tailwind.css';
-import './globals.css'
+import React from "react";
+import { useRouter } from 'next/router';
+import "tailwindcss/tailwind.css";
 
 const Register = () => {
+
+  const router = useRouter();
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+
+    // Perform registration logic here
+
+    // Show alert to inform the user
+    alert('Account successfully created! Please log in.');
+
+    // Redirect to the login page
+    router.push('/login');
+  };
+  
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="p-8 bg-white rounded-lg shadow-md w-96">
-        <h1 className="mb-4 text-3xl font-bold text-center text-green-600">
+        <h1 className="mb-4 text-3xl font-bold text-center text-blue-600">
           Sign Up
         </h1>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSignUp}>
           <div>
             <label
               htmlFor="name"
@@ -22,7 +37,7 @@ const Register = () => {
               type="text"
               id="name"
               name="name"
-              className="w-full p-2 border rounded focus:outline-none focus:border-green-500"
+              className="w-full p-2 border rounded focus:outline-none focus:border-blue-500"
               required
             />
           </div>
@@ -37,7 +52,7 @@ const Register = () => {
               type="email"
               id="email"
               name="email"
-              className="w-full p-2 border rounded focus:outline-none focus:border-green-500"
+              className="w-full p-2 border rounded focus:outline-none focus:border-blue-500"
               required
             />
           </div>
@@ -52,13 +67,13 @@ const Register = () => {
               type="password"
               id="password"
               name="password"
-              className="w-full p-2 border rounded focus:outline-none focus:border-green-500"
+              className="w-full p-2 border rounded focus:outline-none focus:border-blue-500"
               required
             />
           </div>
           <button
             type="submit"
-            className="w-full p-2 text-white bg-green-500 rounded-full hover:bg-green-600 focus:outline-none"
+            className="w-full p-2 text-white bg-blue-500 rounded-full hover:bg-blue-600 focus:outline-none"
           >
             Sign Up
           </button>
